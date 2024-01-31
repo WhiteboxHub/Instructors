5fd70536eea32b773b847e2afc7e85a6f28c5823
WBL-Training <training@whitebox-learning.com>
2024-02-01 02:31:08 +0530

    Delete A_SAI_TEJA directory

--- A_SAI_TEJA/btech_marks.py
+++
@@ -1,4 +0,0 @@
-name = input("enter the name")
-shname = name[-2:]
-for num in range(1,11):
-    print(shname*num)
--- A_SAI_TEJA/hc1.py
+++
@@ -1,3 +0,0 @@
-n=list(map(int,input().split(',')))
-for i in n:
-    print(i,end='')
