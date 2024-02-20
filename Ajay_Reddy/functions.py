72efc1f09b68d926e3fdc6bde53291d3b589d8dd
WBL-Training <training@whitebox-learning.com>
2024-02-21 04:19:11 +0530

    Add files via upload

---
+++ Ajay_Reddy/functions.py
@@ -0,0 +1,5 @@
+def add(a,b):
+    sum = a + b
+    return sum
+result = add(5,4)
+print("the addition is ",result)
--- Ajay_Reddy/pattern.py
+++ Ajay_Reddy/pattern.py
@@ -2,17 +2,17 @@
 print("Square pattern")
 for i in range(4):
     for j in range(4):
-        print("* ",end="")
+        print("$ ",end="")
     print()
     
 #left reacangle triangle
-print("Left Reacangle triangle")
+print("rightangle triangle")
 for i in range(4):
     for j in range(i+1):
         print("* ",end="")
     print()
 #down reacangle triangle
-print("down Reacangle triangle")
+print("down rightangle triangle")
 for i in range(4):
     for j in range(4-i):
         print("* ",end="")
