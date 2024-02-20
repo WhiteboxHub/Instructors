b7c2dc8a06d68d0f943b966b81358be839c5558f
WBL-Training <training@whitebox-learning.com>
2024-02-20 23:04:36 +0530

    Update pattern.py

--- Ajay_Reddy/pattern.py
+++ Ajay_Reddy/pattern.py
@@ -8,6 +8,12 @@ for i in range(4):
 #left reacangle triangle
 print("Left Reacangle triangle")
 for i in range(4):
-    for j in range(i):
+    for j in range(i+1):
         print("* ",end="")
-        print()
+    print()
+#down reacangle triangle
+print("down Reacangle triangle")
+for i in range(4):
+    for j in range(4-i):
+        print("* ",end="")
+    print()
